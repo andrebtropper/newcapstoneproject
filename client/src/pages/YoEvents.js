db68d841011787e25aafcc15acc46d9fa1332a26
Andre Tropper <andrebtropper@gmail.com>
2021-04-09 13:35:23 +0200

    faked it until i made it

--- client/src/pages/YoEvents.js
+++ client/src/pages/YoEvents.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { ReactComponent as EventLogo } from "../icons/yoevents.svg";
 import styled from 'styled-components/macro';
+import { ReactComponent as YOCreatedEvent } from "../images/FakePostImages/createdevent.svg";
+import { ReactComponent as Favorites } from "../images/FakePostImages/fakefavs.svg";
 
 
 export default function YoEvents() {
 
-    const likedEvents = JSON.parse(localStorage.getItem('likedEvents') || '{}');
-    const userEvent = JSON.parse(localStorage.getItem('newEvent') || '{}');
+    // const likedEvents = JSON.parse(localStorage.getItem('likedEvents') || '{}');//
+    //  const userEvent = JSON.parse(localStorage.getItem('newEvent') || '{}');//
 
     const [currentTab, setCurrentTab] = useState('tab1');
     const tabList = [
@@ -15,22 +17,7 @@ export default function YoEvents() {
             label: 'Your Events',
             content: (
                 <YourEventsWrap>
-                    <Title>
-                        {userEvent['event_title']}
-                    </Title>
-                    <LocateDate>
-                        <Date>
-                            {userEvent['event_location']}
-                        </Date>
-                        <Date>
-                            {userEvent['event_month']}
-
-                            {userEvent['event_day']}
-                        </Date>
-                    </LocateDate>
-                    <About>
-                        {userEvent['event_description']}
-                    </About>
+                    <YOCreatedEvent />
                 </YourEventsWrap>
             )
         },
@@ -39,10 +26,9 @@ export default function YoEvents() {
             label: 'Favorites',
             content: (
                 <YourEventsWrap>
-                    <Title>
-                        {likedEvents}
-                    </Title>
+                    <Favorites />
                 </YourEventsWrap>
+
 
             )
         }
@@ -51,39 +37,32 @@ export default function YoEvents() {
     return (
         <div>
             <Category> <EventLogo /> </Category>
-
-            <EventWrapper>
-                <TabNav>
-
-                    {
-                        tabList.map((tab, i) => (
-                            <TabButton
-                                key={i}
-                                onClick={() => setCurrentTab(tab.name)}
-                                className={(tab.name === currentTab) ? 'active' : ''}>
-                                {tab.label}
-                            </TabButton>
-                        ))
-                    }
-                </TabNav>
-
-                {tabList.map((tab, i) => {
-                    if (tab.name === currentTab) {
-                        return <div key={i}>{tab.content}</div>;
-                    } else {
-                        return null;
-                    }
-                })
+            <TabNav>
+                {
+                    tabList.map((tab, i) => (
+                        <TabButton
+                            key={i}
+                            onClick={() => setCurrentTab(tab.name)}
+                            className={(tab.name === currentTab) ? 'active' : ''}>
+                            {tab.label}
+                        </TabButton>
+                    ))
                 }
-            </EventWrapper>
+            </TabNav>
+
+            {tabList.map((tab, i) => {
+                if (tab.name === currentTab) {
+                    return <div key={i}>{tab.content}</div>;
+                } else {
+                    return null;
+                }
+            })
+            }
+
         </div>
     )
 }
 
-const EventWrapper = styled.div`
-border-radius:10px;
-padding-bottom:10px;
-`
 const Category = styled.div`
 display:flex;
 flex-direction:row;
@@ -91,43 +70,12 @@ justify-content:center;
 background:var(--darkblue);
 padding:20px 0px 10px 0px;
 `
-const About = styled.div`
-padding:20px;
-color:var(--mainblue);
-font-family:"Lucida Grande";
-`
-const LocateDate = styled.div`
-display:flex;
-flex-direction:row;
-margin:10px;
-justify-content:center;
-font-family:"Lucida Grande";
-`
-const Date = styled.div`
-background:var(--mainorange);
-color:var(--mainwhite);
-border-radius:5px;
-padding:5px;
-margin:0 5px;
-font-family:"Lucida Grande";
-`
-const Title = styled.label`
-flex-direction:row;
-border-radius:10px;
-background:var(--mainorange);
-color:var(--mainwhite);
-padding:5px;
-text-align:center;
-font-family:"Lucida Grande";
-`
 const YourEventsWrap = styled.div`
-border:var(--mainblue) 2px solid;
-border-radius:10px;
 display:flex;
 flex-direction:column;
-background:white;
-margin:2rem;
-box-shadow: 0.2rem 0.2rem 0.2rem rgba(0,0,0, 35%);
+justify-content:center;
+margin:1.5rem 1rem 1rem 1rem;;
+
 `
 const TabNav = styled.div`
 display:flex;
